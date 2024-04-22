b56ff669ef37f1ca4a149e7afd2f0ef1b1e04211
nerijus <npigulevicius@gmail.com>
2024-04-22 14:25:43 +0300

    forgot interface

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -3,8 +3,10 @@
 namespace App\Providers;
 
 use App\Interfaces\Repositories\JobRepositoryInterface;
+use App\Interfaces\Repositories\JobsIdTrackerRepositoryInterface;
 use App\Repositories\Redis\BaseRepository;
 use App\Repositories\Redis\JobRepository;
+use App\Repositories\Redis\JobsIdTrackerRepository;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\ServiceProvider;
 
@@ -19,6 +21,7 @@ class RepositoryServiceProvider extends ServiceProvider
             ->needs(Cache::class)
             ->give(fn () => Cache::store('redis')); // Change to redis/memcache, or whatever else you want.
         $this->app->bind(JobRepositoryInterface::class, JobRepository::class);
+        $this->app->bind(JobsIdTrackerRepositoryInterface::class, JobsIdTrackerRepository::class);
     }
 
     /**
